8a5e7300bf058615ae163f500a7147818e1670f7
Samantha <sami.shackelford@gmail.com>
2019-10-11 16:52:29 -0700

    Update Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,15 +3,15 @@ import { NavLink, Link } from 'react-router-dom';
 import './Nav.scss';
 
 const Nav = props => {
-    let nav = props.user ?
+    let userNav = props.user ?
         <div className="nav-links">
-            <p className="nav-link">Hi, {props.user.name}</p>
+            <p className="nav-link">Hi, {props.user.name}<i class="fas fa-smile"></i></p>
             <p className="nav-link"></p>
             <NavLink exact to='/' className='nav-link'>View All Tasks</NavLink>
             <p className="nav-link"></p>
             <NavLink exact to='/create-task' className='nav-link'>+ Task</NavLink>
             <p className="nav-link"></p>
-            <NavLink exact to='/month' className='nav-link'>Calendar</NavLink>
+
             <p className="nav-link"></p>
 
             <Link to='/login' onClick={props.handleLogOut}>Log Out</Link>
@@ -27,13 +27,27 @@ const Nav = props => {
     return (
         <div>
             <div className="nav">
-                <Link to='/'><h1 className="logo">VISUALIZE</h1></Link>
+                <div className="logo-link">
+                    <Link to='/'><h1 className="logo">VISUALIZE</h1></Link>
+                </div>
                 <div className="dropdown">
-                    <button className="btn user-dropdown" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <div><i className="far fa-user"></i></div>
-                    </button>
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <button className="dropdown-item">{nav}</button>
+                    <div className="nav-icon-box">
+                        <Link to='/create-task'><i class="fas fa-plus"></i></Link>
+                    </div>
+                    <div className="nav-icon-box">
+                        <Link to='/'><i class="fas fa-list"></i></Link>
+                    </div>
+                    <div className="nav-icon-box">
+                        <Link exact to='/month' className='nav-icon-box calendar-icon'><i class="far fa-calendar-alt"></i></Link>
+                    </div>
+                    <div className="nav-icon-box">
+                        <button className="btn user-dropdown" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <div className="nav-icon-box"><i className="far fa-user"></i></div>
+                        </button>
+
+                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <button className="dropdown-item">{userNav}</button>
+                        </div>
                     </div>
                 </div>
             </div>
