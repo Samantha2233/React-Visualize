97aa874546d5868f8d8e6720fd8661379dc13a82
Samantha <sami.shackelford@gmail.com>
2019-10-11 17:54:07 -0700

    fix styling of tasks on allTasksPage

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,36 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Task.scss';
 
-function Task({ task, handleDeleteTask, handleCompleteTask }) {
-    return (
+class Task extends Component {
+    render() {
+        const task = this.props.task;
+        const handleDeleteTask = this.props.handleDeleteTask;
+        const handleCompleteTaskOnHome = this.props.handleCompleteTaskOnHome;
 
-        <div className={`task-card ${task.color} ${task.size}`}>
-            <input type='checkbox' defaultChecked={task.completed} className='check-box' onClick={() => handleCompleteTask(task._id, !task.completed)} />
+        return (
+            <div className={`task-card ${task.color} ${task.size}`}>
+                <div className="task-header">
+                    <div>
+                        <div className='container'>
+                            <input type='checkbox' defaultChecked={task.completed} className='check-box' onClick={() => handleCompleteTaskOnHome(task._id, !task.completed)} />
+                            <span class='check-box'></span>
+                            <label> Complete?</label>
+                        </div>
+                    </div>
+                    <div>
+                        <Link className="btn btn-xs" to={{ pathname: '/update', state: { task } }}>
+                            <i class="fas fa-pencil-alt task-icon"></i>
+                        </Link>
+                        <button className="btn btn-xs" onClick={() => handleDeleteTask(task._id)}>
+                            <i class="fas fa-trash-alt task-icon"></i>
+                        </button>
+                    </div>
+                </div>
+                <h2 className='task-name'>{task.name}</h2>
 
-            <h3 className='task-name'>{task.name}</h3>
-            <div className='task-body'>
-                <div>{task.completed}</div>
+
+                <div className='task-body'>
+                    <div>{task.completed}</div>
+                </div>
+
             </div>
-            <div className-="task-footer">
-                <Link
-                    className="btn btn-xs"
-                    to={{
-                        pathname: '/update',
-                        state: { task }
-                    }}
-                >
-                    <i class="fas fa-pencil-alt"></i>
-                </Link>
-                <button
-                    className="btn btn-xs btn-danger"
-                    onClick={() => handleDeleteTask(task._id)}
-                >
-                    <i class="fas fa-trash-alt"></i>
-                </button>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Task;
--- src/pages/AllTasksPage/AllTasksPage.jsx
+++ src/pages/AllTasksPage/AllTasksPage.jsx
@@ -1,29 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
 import Task from '../../components/Task/Task';
 import './AllTasksPage.scss';
 
-function AllTasksPage(props) {
-    return (
-        <>
-            <div className="background">
-                <div className='all-tasks-header'>
-                    <h1>All Tasks</h1>
-                    <Link to='/create-task'><button> + </button></Link>
+class AllTasksPage extends Component {
+    render() {
+
+
+
+        // const tasks = this.props.tasks;
+        // const completeTasks = tasks.filter(task => task.completed === true);
+        // const incompleteTasks = tasks.filter(task => task.completed === false);
+
+        return (
+            <>
+                <div className="background">
+                    <div className='all-tasks-header'>
+                        <h1>All Tasks</h1>
+                        <p class="task-link">All</p>
+                        <p class="task-link">Completed</p>
+                        <p class="task-link">Incomplete</p>
+                    </div>
+                    <div className='AllTasksPage'>
+                        {this.props.tasks.map(task =>
+                            <Task
+                                key='task._id'
+                                task={task}
+                                handleDeleteTask={this.props.handleDeleteTask}
+                                handleCompleteTaskOnHome={this.props.handleCompleteTaskOnHome}
+                                // completeTasks={completeTasks}
+                                // incompleteTasks={incompleteTasks}
+                                match={this.props.match}
+                            />
+                        )}
+                    </div>
+
+
+
+
                 </div>
-                <div className='AllTasksPage'>
-                    {props.tasks.map(task =>
-                        <Task
-                            key='task._id'
-                            task={task}
-                            handleDeleteTask={props.handleDeleteTask}
-                            handleCompleteTask={props.handleCompleteTask}
-                        />
-                    )}
-                </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 }
+
 
 export default AllTasksPage;
